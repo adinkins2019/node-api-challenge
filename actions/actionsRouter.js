17f8a34e690c63acb109aa4dadb3309f895e07bc
Antonio Dinkins <afdinki@gmail.com>
2020-06-14 21:57:43 -0400

    Modified projectsRouter and actionsRouter to add put functionality

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -16,28 +16,43 @@ router.get('/:id', (req, res) => {
             res.status(404).json({message: "User not found"})
         }
     })
+    .catch(error => {
+        res.status(500).json({errorMessage: "An error has occurred. Please try again at a later time."})
+    })
 })
 
 router.post('/', (req, res) => {
+    if(req.body.project_id && req.body.description && req.body.notes){
+        const action = {project_id: req.body.project_id, description: req.body.description, notes: req.body.notes, completed: req.body.completed}
+        actions.insert(action)
+    } else {
+        res.status(400).json({message: "Invalid data"})
+    }
 
 })
 
 router.put('/:id', (req, res) => {
-    const user = get(req.params.id)
+    const user = actions.get(req.params.id)
     .then(user => {
         if(user){
-            
+            const updatedActions = actions.insert(req.params.id, {
+                project_id: req.body.project_id,
+                description: req.body.description,
+                notes: req.body.notes,
+                complete: req.body.completed
+            })
+            res.json(updatedActions)
         } else {
             res.status(404).json({message: "Specified user was not found"})
         }
     })
     .catch(error => {
-
+        res.status(500).json({errorMessage: "A server error has occurred. Please try again at a later time."})
     })
 })
 
 router.delete('/:id', (req, res) => {
-    const user = get(req.params.id)
+    const user = actions.get(req.params.id)
     .then(user => {
         if(user){
             actions.remove(req.params.id)
